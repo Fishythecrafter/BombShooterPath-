025d8590bce9881e6108e34f9616f3df8f088cbc
Fishythecrafter <108023439+Fishythecrafter@users.noreply.github.com>
2023-05-08 17:08:45 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,13 +2,13 @@ namespace FourthPath;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "36.1";
+    public const string WorksOnVersion = "36.3";
     public const string Version = "1.0.0";
-    public const string Name = "FourthPath";
+    public const string Name = "Path++ BombShooter";
 
-    public const string Description = "An empty mod";
+    public const string Description = "More Path BombShooter";
     public const string Dependencies = "doombubbles/paths-plus-plus";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "FishyTheCrafter"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "FishyTheCrafter"; // TODO add your repo name here, also in the download url in README.md
 }
